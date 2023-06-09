a2938c2c6a8aceeb5cf2cd7aca215f3b9ddcc84d
lin <2423860442@qq.com>
2023-06-09 10:41:02 +0800

    Initial Commit

---
+++ WeiBoSwiftUI/WeiBoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiBoSwiftUIApp.swift
+//  WeiBoSwiftUI
+//
+//  Created by Estim on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct WeiBoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
